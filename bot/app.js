32a12804c35f488232d4b40e3b48246511d39551
TheOtherGrimley <adamgrimley@outlook.com>
2018-03-24 19:45:59 +0000

    Adult works, the rest don't

--- bot/app.js
+++ bot/app.js
@@ -6,6 +6,7 @@ var bodyParser = require('body-parser');
 var botMiddleware = require('./middleware');
 const app = express();
 var router = express.Router();
+var attractions = require("./Attractions.json")
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.json({ limit: '100mb' }));
@@ -36,12 +37,73 @@ app.post('/api/messages', connector.listen());
 var inMemoryStorage = new builder.MemoryBotStorage();
 var bot = new builder.UniversalBot(connector).set('storage', inMemoryStorage);
 
-var recognizer = new builder.LuisRecognizer(process.env.LUIS_MODEL_URL);
+var recognizer = new builder.LuisRecognizer("https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/f7c8e57c-b65e-4df4-b99f-8ca353ccc152?subscription-key=c42114c8555e4734b647600c989e3d58&verbose=true&timezoneOffset=0&q=");
 bot.recognizer(recognizer);
 
 bot.dialog('/', function(session){
     session.send('Could you try saying that in Pirate? My English is a bit rusty.');
 });
+
+var SelectedAttraction;
+bot.dialog('Price', 
+    function(session, args) {
+        session.beginDialog('findTicketPrice', args)
+    }).triggerAction({
+        matches:'Price'
+    });
+        
+bot.dialog('findTicketPrice', [
+    function(session, args){
+    var att = builder.EntityRecognizer.findEntity(args.intent.entities, 'TourismAttractions').entity;
+        //if(builder.EntityRecognizer.findEntity(args.intent.entities, 'Tickets').entity !== null)
+            //var ticket = builder.EntityRecognizer.findEntity(args.intent.entities, 'Tickets').entity;
+
+        var found = false;
+        if(att){
+            //session.send(att)
+            for (var i = 0; i < attractions.Attractions.length; i++){
+                if(att === attractions.Attractions[i].Name){
+                    found = true;
+                    session.send("Found");
+                    //if(ticket){
+                      //  if(ticket === 'Concession')
+                        //    session.endDialogWithResult("Ticket price is " + attractions.Attractions[i].Concession)
+                        //if(ticket === 'Adult')
+                        //    session.endDialogWithResult("Ticket price is " + attractions.Attractions[i].Adult)
+                        //if(ticket === 'Child')
+                        //    session.endDialogWithResult("Ticket price is " + attractions.Attractions[i].Child)
+                    //}
+                    //else{
+                        session.conversationData.att = attractions.Attractions[i];
+                        builder.Prompts.text(session, "What ticket type are you interested in? (Adult/Child/Concession)")
+                        //}
+                    }
+                }
+                if(found === false){
+                    //session.send(att.getText());
+                    session.send("We could not find that attraction in our list.");
+                }
+            }
+            else{
+                session.endConversation("Could not understand you, try again.")
+            }
+        },
+        function(session, results){
+            if(results.response === "Adult" || results.response === "adult"){
+                session.endConversation("Ticket price is £" + session.conversationData.att.Adult)
+            }
+            else if(results.response === "Child" || results.response === "child"){
+                session.endConversation("Ticket price is £" + session.conversationData.att.Child)
+            }
+            else if(results.response === "Concession" || results.response === "concession"){
+                session.endConversation("Ticket price is £" + session.conversationData.att.Concession)
+            }
+            else{
+                session.endConversation("Could not find that ticket")
+            }
+        }
+])
+
 
 bot.on('conversationUpdate', function (message) {
     if (message.membersAdded) {
